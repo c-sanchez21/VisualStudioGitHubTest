604603b8980567758a9f3114c8e1febc9eddbd60
Z <Z@Z-PC>
2016-12-26 12:31:15 -0800

    Folder Added Outside VS

---
+++ AddFolderTest/AddFolderTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddFolderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project folder will be moved outside of visual studio");
+        }
+    }
+}
